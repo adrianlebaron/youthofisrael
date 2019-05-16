903973326b0b4afc0d2831b4f504975a53f45ecf
A.D <adrianlebaron@gmail.com>
2019-05-16 10:58:57 -0600

    changed heading

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,8 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>YOI</h1>
+        <h2>Welcome to the 2019 youth of Israel come back for more updates soon</h2>
       </div>
     );
   }
